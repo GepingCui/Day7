1ccee1d83c0f2adb708fce905a121ae6a41313ee
GepingCui <101596309+GepingCui@users.noreply.github.com>
2022-03-25 10:24:14 -0700

    Add files via upload

---
+++ Color Ball.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace _ColorBall
+{
+
+
+    public class Color
+    {
+        public int RED { set; get; }
+        public int GREEN { set; get; }
+        public int BLUE { set; get; }
+        public int ALPHA { set; get; }
+
+        public Color(int red, int green, int blue, int alpha)
+        {
+            this.RED = red;
+            this.GREEN = green;
+            this.BLUE = blue;
+            this.ALPHA = alpha;
+        }
+
+        public Color(int red, int green, int blue)
+        {
+            this.RED = red;
+            this.GREEN = green;
+            this.BLUE = blue;
+            this.ALPHA = 255;
+        }
+        public int GrayScale()
+        {
+            return (this.RED + this.GREEN + this.BLUE) / 3;
+        }
+
+    }
+
+    public class Ball
+    {  
+        
+        public Color Color { set; get; }
+        public int Size;
+        public int ThrownTime;
+
+        public Ball(int size,Color color)
+        {
+            this.Size=size;
+            this.Color=color;
+            this.ThrownTime=0;
+        }
+
+        public void Pop()
+        {
+            this.Size=0;
+        }
+
+        public void Throw()
+        {
+            if (this.Size > 0)
+            {
+                this.ThrownTime++;
+            }
+        }
+
+        public int ThrownNumber()
+        {
+            return this.ThrownTime;
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Color CL1 = new Color(15,30,45,60);
+            Ball Ball1 = new Ball(15,CL1);
+
+            Ball1.Throw();
+            Console.WriteLine(Ball1.ThrownNumber());
+            Ball1.Throw();
+            Ball1.Throw();
+            Ball1.Throw();
+            Ball1.Throw();
+            Ball1.Throw();
+            Ball1.Throw();
+            Console.WriteLine(Ball1.ThrownNumber());
+            Ball1.Pop();
+            Console.WriteLine(Ball1.ThrownNumber());
+            Ball1.Throw();
+            Console.WriteLine(Ball1.ThrownNumber());
+        }
+    }
+
+}
---
+++ Designing and Building Classes.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace _OOP
+ {
+
+
+    public abstract class Person
+    {
+        public DateTime BirthDay;
+        public decimal Salary;
+        public string Address;
+
+        public int GetAge()
+        {
+            DateTime now = DateTime.Now;
+            int age = now.Year - BirthDay.Year;
+            return age;
+        }
+
+        public abstract decimal GetSalary();
+
+        public void UpdateAddress(string NewAdd)
+        {
+            Address = NewAdd;
+        }
+    }
+
+    public class Instructor : Person
+    {
+        public string Department;
+        public Boolean ifHead;
+        public DateTime JoinDay;
+
+        public override decimal GetSalary()
+        {
+            if (Salary >= 0)
+            {
+                DateTime now = DateTime.Now;
+                int experience = now.Year - JoinDay.Year;
+
+                return Salary*(1+experience*(decimal)0.05);
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+    }
+
+
+    public class Student : Person
+    {
+        public string[] course;
+        public string[] grades;
+
+        public override decimal GetSalary()
+        {
+            return 0;
+
+        }
+        public double GetGPA()
+        {
+            int Grade = 0;
+            foreach(string item in grades)
+            {
+                if (item == "A")
+                {
+                    Grade += 4;
+                }else if (item == "B")
+                {
+                    Grade += 3;
+                }
+                else if (item == "C")
+                {
+                    Grade += 2;
+                }
+                else if (item == "D")
+                {
+                    Grade += 1;
+                }
+
+            }
+            return (double)Grade/grades.Length;
+        }
+    }
+
+
+    public class Course
+    {
+        public string[] Students;
+    }
+
+
+    public class Department
+    {
+        public string HeadInstructor;
+        public double Budget;
+        public DateTime StartDtime;
+        public DateTime EndDtime;
+        public string[] Courses;
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Student = new Student();
+            Person Instructor = new Instructor();
+        }
+    }
+
+}
